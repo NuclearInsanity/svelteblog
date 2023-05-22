446dffb45480dff1d6f92776f8bc8b92152c5045
NuclearInsanity <110328979+NuclearInsanity@users.noreply.github.com>
2023-05-22 19:17:21 +0800

    feat: Cache

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,5 +1,8 @@
 import * as posts from '$lib/services/posts'
 
-export const load = async ({ params }) => {
+export const load = async ({ setHeaders, params }) => {
+	setHeaders({
+		'Cache-control': `max-age=0, s-maxage=${60*60}`
+	})
 	return { post: await posts.getPost(params.slug) }
 }
